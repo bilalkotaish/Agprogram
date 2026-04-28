e69d700c35f4e9bacce9465eaea1ed4af222a92c
Bilalkotaish <you@example.com>
2026-04-28 12:50:23 +0300

    fixed some errors

--- frontends/src/api.js
+++ frontends/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://agbackend-1-hi8l.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://agbackend-78.onrender.com/api',
   withCredentials: true,
 });
 
